43ae400714a2d6c4d83f1d9ff1e12ed0b74621a1
Tamara1608 <79335848+Tamara1608@users.noreply.github.com>
2021-02-19 21:25:54 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,48 @@
+function myFunction() {
+
+    var model = document.getElementById("1").textContent;
+    var cena = document.getElementById("2").textContent;
+
+    alert(model + '' + cena);
+
+}
+
+function myFunction1() {
+
+    var model = document.getElementById("3").textContent;
+    var cena = document.getElementById("4").textContent;
+
+    alert(model + '' + cena);
+}
+
+function myFunction2() {
+
+    var model = document.getElementById("5").textContent;
+    var cena = document.getElementById("6").textContent;
+
+    alert(model + '' + cena);
+}
+
+function myFunction3() {
+
+    var model = document.getElementById("7").textContent;
+    var cena = document.getElementById("8").textContent;
+
+    alert(model + '' + cena);
+}
+
+function myFunction4() {
+
+    var model = document.getElementById("9").textContent;
+    var cena = document.getElementById("10").textContent;
+
+    alert(model + '' + cena);
+}
+
+function myFunction5() {
+
+    var model = document.getElementById("11").textContent;
+    var cena = document.getElementById("12").textContent;
+
+    alert(model + '' + cena);
+}
